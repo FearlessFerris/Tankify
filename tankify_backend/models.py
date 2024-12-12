f554f5336d8667dbbc36b1ca106afe15127cb190
Marcus <marcus.perry2017@gmail.com>
2024-12-11 21:25:38 -0500

    Modifying Tank Model

--- tankify_backend/models.py
+++ tankify_backend/models.py
@@ -144,7 +144,7 @@ class User( Base ):
         return None 
 
 
-class Tank( Base ): 
+class Tank( Base ):
     """ Tank Model """
 
     __tablename__ = 'tanks'
@@ -153,72 +153,145 @@ class Tank( Base ):
     tag = Column( String, nullable = False )
     description = Column( String, nullable = False )
     price = Column( String, nullable = False )
-    type = Column( String, nullable = False )
     nation = Column( String, nullable = False )
     nation_flag = Column( String, nullable = False )
-    tier = Column( String, nullable = False )
-    carousel_image = Column( String, nullable = False )
-    hd_image = Column( String, nullable = False )
-    created_at = Column( DateTime, server_default = func.now() )
+    image = Column( String, nullable = False )
+    crew = Column( object, nullable = False )
+    default_profile = Column( object, nullable = False )
+    guns = Column( object, nullable = False )
+    modules_tree = Column( object, nullable = False )
+    next_tanks = Column( object, nullable = False )
+    radios = Column( object, nullable = False )
+    suspensions = Column( object, nullable = False )
+    turrets = Column( object, nullable = False )
 
-    # Relationships 
+    def __init__( self, name, tag, description, price, nation, nation_flag, image, crew, default_profile, guns, modules_tree, next_tanks, radios, suspensions, turrets ):
+        """ Initialize Tank Class """
 
-
-    # Instance Methods 
-    def __init__( self, name, tag, description, price, type, nation, nation_flag, tier, hd_image, carousel_image ): 
         self.name = name 
         self.tag = tag 
         self.description = description
-        self.price = price 
-        self.type = type
-        self.nation = nation
-        self.nation_flag = nation_flag
-        self.tier = tier
-        self.hd_image = hd_image 
-        self.carousel_image = carousel_image
+        self.price = price
+        self.nation = nation 
+        self.nation_flag = nation_flag 
+        self.image = image 
+        self.crew = crew 
+        self.default_profile = default_profile 
+        self.guns = guns
+        self.modules_tree = modules_tree 
+        self.next_tanks = next_tanks 
+        self.radios = radios 
+        self.suspensions = suspensions 
+        self.turrets = turrets 
 
-    def show_info( self ): 
-        """ Retrieves Tank Information """
+    def __repr__( self ): 
+        """ Representation Method for Tank Instance """
 
         tank = {
             'id': str( self.id ),
             'name': self.name,
-            'tag': self.tag,
             'description': self.description,
             'price': self.price,
-            'type': self.type, 
-            'nation': self.nation, 
-            'nation_flag': self.nation_flag, 
-            'tier': self.tier,
-            'hd_image': f'{ WOT_CDN_BASE }{ self.tag.lower() }/{ self.tag.lower() }_image.png',
-            'carousel_image': self.carousel_image,
-
+            'nation': self.nation,
+            'nation_flag': self.nation_flag,
+            'image': f'{ WOT_CDN_BASE }{ self.tag.lower() }/{ self.tag.lower() }_image.png', 
+            'crew': self.crew,
+            'default_profile': self.default_profile,
+            'guns': self.guns,
+            'modules_tree': self.modules_tree,
+            'next_tanks': self.next_tanks,
+            'radios': self.radios,
+            'suspensions': self.suspensions,
+            'turrets': self.turrets
         }
-        return tank 
-    
-    # Class Methods 
+
+        return tank
+
     @classmethod
-    def get_all_tanks( cls ): 
-        """ Retrieves all Tanks from Database """
+    def all_tanks( cls ):
+        """ Retrieve all Tanks """
 
         tanks = cls.query.all()
-        tank_list = [ tank.show_info() for tank in tanks ]
+        tank_list = [ tank.__repr__() for tank in tanks ]
         return tank_list
+    
+     
+
+# class Tank( Base ): 
+#     """ Tank Model """
+
+#     __tablename__ = 'tanks'
+#     id = Column( UUID( as_uuid = True ), primary_key = True, default = uuid.uuid4 )
+#     name = Column( String, nullable = False )
+#     tag = Column( String, nullable = False )
+#     description = Column( String, nullable = False )
+#     price = Column( String, nullable = False )
+#     type = Column( String, nullable = False )
+#     nation = Column( String, nullable = False )
+#     nation_flag = Column( String, nullable = False )
+#     tier = Column( String, nullable = False )
+#     carousel_image = Column( String, nullable = False )
+#     hd_image = Column( String, nullable = False )
+#     created_at = Column( DateTime, server_default = func.now() )
+
+#     # Relationships 
 
 
-class Transaction( Base ):
-    """ Transaction Model """
+#     # Instance Methods 
+#     def __init__( self, name, tag, description, price, type, nation, nation_flag, tier, hd_image, carousel_image ): 
+#         self.name = name 
+#         self.tag = tag 
+#         self.description = description
+#         self.price = price 
+#         self.type = type
+#         self.nation = nation
+#         self.nation_flag = nation_flag
+#         self.tier = tier
+#         self.hd_image = hd_image 
+#         self.carousel_image = carousel_image
 
-    __tablename__ = 'transactions'
-    id = Column( UUID( as_uuid = True ), primary_key = True, default = uuid.uuid4 )
-    user_id = Column( UUID( as_uuid = True ), ForeignKey( 'users.id' ), nullable = False )
-    tank_id = Column( UUID( as_uuid = True ), ForeignKey( 'tanks.id' ), nullable = False )
-    amount = Column( Integer, nullable = False )  
-    timestamp = Column( DateTime, server_default = func.now() )
+#     def show_info( self ): 
+#         """ Retrieves Tank Information """
 
-    # Relationships with back_populates
-    user = relationship( 'User', back_populates='transactions' )
-    tank = relationship( 'Tank', backref='transactions' )
+#         tank = {
+#             'id': str( self.id ),
+#             'name': self.name,
+#             'tag': self.tag,
+#             'description': self.description,
+#             'price': self.price,
+#             'type': self.type, 
+#             'nation': self.nation, 
+#             'nation_flag': self.nation_flag, 
+#             'tier': self.tier,
+#             'hd_image': f'{ WOT_CDN_BASE }{ self.tag.lower() }/{ self.tag.lower() }_image.png',
+#             'carousel_image': self.carousel_image,
+
+#         }
+#         return tank 
+    
+#     # Class Methods 
+#     @classmethod
+#     def get_all_tanks( cls ): 
+#         """ Retrieves all Tanks from Database """
+
+#         tanks = cls.query.all()
+#         tank_list = [ tank.show_info() for tank in tanks ]
+#         return tank_list
+
+
+# class Transaction( Base ):
+#     """ Transaction Model """
+
+#     __tablename__ = 'transactions'
+#     id = Column( UUID( as_uuid = True ), primary_key = True, default = uuid.uuid4 )
+#     user_id = Column( UUID( as_uuid = True ), ForeignKey( 'users.id' ), nullable = False )
+#     tank_id = Column( UUID( as_uuid = True ), ForeignKey( 'tanks.id' ), nullable = False )
+#     amount = Column( Integer, nullable = False )  
+#     timestamp = Column( DateTime, server_default = func.now() )
+
+#     # Relationships with back_populates
+#     user = relationship( 'User', back_populates='transactions' )
+#     tank = relationship( 'Tank', backref='transactions' )
 
 
 # class Review( Base ):
